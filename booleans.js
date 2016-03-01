f076733796e0eb2f4d214f6be50de8fb6fe0497e
Sylvia Maguiña <SMag23@gmail.com>
2016-02-29 18:51:57 -0800

    Booleans tutorial examples

---
+++ booleans.js
@@ -0,0 +1,51 @@
+console.log(5 > 1)
+
+console.log(5 < 1)
+
+// false
+"z" < "a"
+
+"Z" < "z"
+
+"a" > "A"
+
+//comparison operators
+>= (greater than or equal to)
+<= (less than or equal to)
+== (equal to)
+!= (not equal to)
+
+//here the outcome is rather easy to predict
+console.log("A" != "a")
+
+console.log("A" == "A")
+
+//logical operators
+&& logical and
+|| logical or
+! logical not
+
+console.log(true && true)
+
+console.log(false || false)
+
+
+console.log(true || A)
+
+console.log(false && A)
+
+
+
+//Fun - Trick questions
+
+console.log(typeof NaN)
+
+console.log(NaN == NaN)
+
+
+console.log(true || A)
+
+console.log(false && A)
+
+
+console.log("b" && "B")
