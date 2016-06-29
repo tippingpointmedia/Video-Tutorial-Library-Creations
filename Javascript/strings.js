79de3ea81f9704a47b3bd51bc11b9bf9ce97da44
Sylvia Maguiña <SMag23@gmail.com>
2016-06-28 21:18:15 -0700

    Strings tutorial draft version 1

---
+++ Javascript/strings.js
@@ -0,0 +1,20 @@
+// String sentence example. You can use single or double quotes.
+"This is a tutorial on strings"
+'This is a sentence'
+
+// Incorrect use of strings with syntax error
+var a = "This is a sample "sentence" here"
+
+// Backslash to create a new line, or escape the character
+"This is line one\nAnd this is the second line"
+
+// Backslash with a t to tab or create a space
+"This is line one\tAnd this is the second line"
+
+// Turn special characters (i.e quotes, numerical signs, slashes, apostrophe etc.) into string characters
+"This is a test \"that\" the backslashes won\'t appear."
+"Here is another wacky test\! Here is a backslash... \\"
+
+// Concatenation, or gluing strings together
+"water" + "melon"
+"water" + "melon " + "French" + "Fries"
