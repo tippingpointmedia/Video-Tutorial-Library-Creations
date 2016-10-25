e79312d38bc8b4f473c9f18429f091386ef9b857
Sylvia Maguiña <SMag23@gmail.com>
2016-10-24 21:03:20 -0700

    Create functions.js

---
+++ Javascript/functions.js
@@ -0,0 +1,8 @@
+// Functions are data types that represents programs wrapped in values
+alert("This is a function")
+
+// Values given to functions are called arguments
+alert("This is the argument")
+
+var a = 10;
+console.log("the value of a is", a);
